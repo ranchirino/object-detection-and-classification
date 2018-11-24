513e9832c72dc6cfb12f497a01ae39aed3444157
ranchirino <ranchirino@gmail.com>
2018-11-24 11:15:50 -0600

    _

--- image_object_detection.py
+++ image_object_detection.py
@@ -25,7 +25,9 @@ from pathlib import Path
 
 # This is needed since the notebook is stored in the object_detection folder.
 # sys.path.append("..")
+# from tf_models.research import object_detection
 from object_detection.utils import ops as utils_ops
+
 
 if StrictVersion(tf.__version__) < StrictVersion('1.9.0'):
   raise ImportError('Please upgrade your TensorFlow installation to v1.9.* or later!')
