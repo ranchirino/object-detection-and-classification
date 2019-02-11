73fc2a23e0f12f302b2ceede2952247aed496678
ranchirino <ranchirino@gmail.com>
2019-02-11 11:15:50 -0600

    Update img_faster_rcnn_inception_v2.py

--- img_faster_rcnn_inception_v2.py
+++ img_faster_rcnn_inception_v2.py
@@ -28,21 +28,36 @@ with tf.Session(graph=detection_graph) as sess:
     print('Model loaded')
 
 
+#%% Paths
 PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+PATH_TO_LABELS_PASCAL = os.path.join('data', 'pascal_label_map_v2.pbtxt')
 category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+categories_pascal = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS_PASCAL, use_display_name=True)
+index_pascal_categ = np.sort(list(categories_pascal))
 
-EVAL_PATH = 'evaluate\coco'
-ANN_PATH = os.path.join(EVAL_PATH, 'annotations')
-IMG_PATH = os.path.join(EVAL_PATH, 'images')
-RES_PATH = os.path.join(EVAL_PATH, 'results')
-ann_file_name = 'instances_val2017.json'
-ann_file = os.path.join(ANN_PATH, ann_file_name)
+IMG_PATH = 'C:\RANGEL\GitHub\evaluation_images'
+IMG_PATH_PASCAL = os.path.join(IMG_PATH, 'pascal')
+ANN_PATH_PASCAL = os.path.join(IMG_PATH_PASCAL, 'annotations')
+
+EVAL_PATH = 'evaluate'
+EVAL_PATH_PASCAL = os.path.join(EVAL_PATH, 'pascal')
+
+IMG_SETS_PASCAL = os.path.join(EVAL_PATH_PASCAL, 'image_sets\main')
+RES_PATH_PASCAL = os.path.join(EVAL_PATH_PASCAL, 'results')
+
+# ann_file_name = 'instances_val2017.json'
+# ann_file = os.path.join(ANN_PATH, ann_file_name)
 
 # initialize COCO api
-coco = COCO(ann_file)
+# coco = COCO(ann_file)
 
-PATH_TO_TEST_IMAGES_DIR = os.path.join(IMG_PATH, 'val2017')
+PATH_TO_TEST_IMAGES = os.path.join(IMG_PATH_PASCAL, 'voc2012')
 
+
+#%%
+val_set = open(os.path.join(IMG_SETS_PASCAL, 'val.txt'), "r")
+image_ids = [id[:-1] for id in val_set]
+image_files = [img + '.jpg' for img in image_ids]
 
 with detection_graph.as_default():
     config = tf.ConfigProto()
@@ -65,10 +80,12 @@ with detection_graph.as_default():
 
         total_results = []
         t0 = datetime.now()
-        for img in os.listdir(PATH_TO_TEST_IMAGES_DIR)[:1]:
+        total_imgs = len(image_ids)
+        # for img in os.listdir(PATH_TO_TEST_IMAGES)[:1]:
+        for i, img_id in enumerate(image_ids):
             tf0 = datetime.now()
-            image_id = coco.imgToId[img]
-            image_path = os.path.join(PATH_TO_TEST_IMAGES_DIR, img)
+            # image_id = coco.imgToId[img]
+            image_path = os.path.join(PATH_TO_TEST_IMAGES, img_id + '.jpg')
 
             image = Image.open(image_path)
             image_np = np.array(image)
@@ -76,6 +93,7 @@ with detection_graph.as_default():
             if len(image_np.shape) == 2:
                 image_np = np.stack((image_np, image_np, image_np), axis=2)
 
+            print('Processed images: %d/%d' % (i + 1, total_imgs))
             # run inference
             output_dict = sess.run(tensor_dict,
                                    feed_dict={image_tensor: np.expand_dims(image_np, 0)},
@@ -83,23 +101,27 @@ with detection_graph.as_default():
                                    run_metadata=run_metadata)
 
             # all outputs are float32 numpy arrays, so convert types as appropriate
-            detections = output_dict['num_detections'] = int(output_dict['num_detections'][0])
-            classes = output_dict['detection_classes'] = output_dict['detection_classes'][0].astype(np.uint8)
-            boxes = output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
-            scores = output_dict['detection_scores'] = output_dict['detection_scores'][0]
+            detections = int(output_dict['num_detections'][0])
+            classes = output_dict['detection_classes'][0].astype(np.uint8)
+            boxes = output_dict['detection_boxes'][0]
+            scores = output_dict['detection_scores'][0]
 
-        #     for r in results.get_results_by_score_threshold(
-        #             image_np,
-        #             image_id,
-        #             classes,
-        #             boxes,
-        #             scores,
-        #             min_score_thresh=0.3):
-        #         total_results.append(r)
+            for r in results.get_pascal_results_by_score_threshold(
+                    image_np,
+                    img_id,
+                    classes,
+                    boxes,
+                    scores,
+                    index_pascal_categ,
+                    min_score_thresh=0.5):
+                total_results.append(r)
+
+        results.create_pascal_result_files_by_categories(total_results, categories_pascal, RES_PATH_PASCAL)
+
         #
         # json_file_name = 'faster_rcnn_inception_v2.json'
         #
-        # results.create_json_results_file(
+        # results.create_results_coco_file(
         #     total_results,
         #     os.path.join(RES_PATH, json_file_name))
 
@@ -112,16 +134,16 @@ with detection_graph.as_default():
             #     f.write(trace)
 
 
-            image_np = visualize.visualize_boxes_and_labels(
-                image_np,
-                boxes,
-                classes=classes,
-                scores=scores,
-                category_index=category_index,
-                line_width=3,
-                label_size=13,
-                min_score_thresh=0.6,
-                skip_labels_and_scores=False)
-
-            plt.imshow(image_np)
-            plt.show()
+            # image_np = visualize.visualize_boxes_and_labels(
+            #     image_np,
+            #     boxes,
+            #     classes=classes,
+            #     scores=scores,
+            #     category_index=category_index,
+            #     line_width=3,
+            #     label_size=13,
+            #     min_score_thresh=0.6,
+            #     skip_labels_and_scores=False)
+            #
+            # plt.imshow(image_np)
+            # plt.show()
